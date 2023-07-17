74c0ea0214583723f0d1022be070b249499230aa
nimairoy <nimairoy718@gmail.com>
2023-07-17 18:51:03 +0600

    room card added

--- client/src/components/Rooms/Rooms.jsx
+++ client/src/components/Rooms/Rooms.jsx
@@ -3,6 +3,7 @@ import Container from '../Shared/Container';
 import Card from './Card';
 import Loader from '../Shared/Loader/Loader';
 import { useSearchParams } from 'react-router-dom';
+import Heading from '../Heading/Heading';
 
 const Rooms = () => {
     const [params, setParams] = useSearchParams()
@@ -16,11 +17,11 @@ const Rooms = () => {
         fetch('./rooms.json')
             .then(res => res.json())
             .then(data => {
-                if(category){
+                if (category) {
                     const filtered = data.filter(room => room.category === category);
                     setRooms(filtered);
                 }
-                else{
+                else {
                     setRooms(data)
                 }
             })
@@ -30,18 +31,32 @@ const Rooms = () => {
     }, [category])
 
 
-    if(loading){
+    if (loading) {
         <Loader />
     }
 
 
     return (
         <Container>
-            <div className="grid pt-12 sm:gap-0 md:gap-3 lg:gap-4 xl:gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
-                {
-                    rooms.map((room, index) => <Card room={room} key={index} />)
-                }
-            </div>
+            {rooms && rooms.length > 0
+
+                ?
+
+                <div className="grid pt-12 sm:gap-0 md:gap-3 lg:gap-4 xl:gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+                    {
+                        rooms.map((room, index) => <Card room={room} key={index} />)
+                    }
+                </div>
+
+                :
+                <div className='pt-16'>
+                    <Heading
+                        title={'No Rooms Available In This Category !'}
+                        subtitle={'Please Select Another Categories.'}
+                        center={true}
+                    />
+                </div>
+            }
         </Container>
     );
 };
